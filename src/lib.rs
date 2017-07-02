de9fbbf58e13f28983595bb50e6e186a4d1c1147
Daniel Lockyer <thisisdaniellockyer@gmail.com>
2017-07-02 14:49:49 +0100

    Prepare for first release

---
+++ src/lib.rs
@@ -0,0 +1,18 @@
+#![no_std]
+
+const LOOKUP: [usize; 12] = [0, 3, 2, 5, 0, 3, 5, 1, 4, 6, 2, 4];
+
+pub fn dow(y: usize, m: usize, d: usize) -> usize {
+    let mut yy = y;
+
+    if m >= 3 {
+        yy -= 1;
+    }
+
+    (yy + yy / 4 - yy / 100 + yy / 400 + LOOKUP[m - 1] + d) % 7
+}
+
+#[test]
+fn it_works() {
+    assert_eq!(dow(2017, 7, 2), 6);
+}
