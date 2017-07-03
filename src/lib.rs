dc968d60bd3577b917c0d81ff8e17be4504ce11c
Daniel Lockyer <thisisdaniellockyer@gmail.com>
2017-07-03 15:20:08 +0100

    Add documentation

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,28 @@
 #![no_std]
 
+/// Lookup table for day indices.
 const LOOKUP: [usize; 12] = [0, 3, 2, 5, 0, 3, 5, 1, 4, 6, 2, 4];
 
+/// Return the index of the day for which the given date corresponds to.
+///
+/// # Arguments
+/// 
+/// * `y`: Year
+/// * `m`: Month
+/// * `d`: Day
+///
+/// # Example
+///
+/// ```
+/// extern crate dow;
+///
+/// let day_index = dow::dow(2017, 7, 2);
+/// assert_eq!(day_index, 0); // 2017/07/02 is a Sunday
+/// ```
+///
+/// # Note
+///
+///	Sunday is `0`, Monday is `1` etc.
 pub fn dow(y: usize, m: usize, d: usize) -> usize {
     let mut yy = y;
 
