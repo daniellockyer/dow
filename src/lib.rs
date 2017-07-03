377f7c405aad83b8f7d16e0eb833ed2dd475e031
Jim Rybarski <jim@rybarski.com>
2017-07-03 13:57:01 -0500

    Use enum to represent days

--- src/lib.rs
+++ src/lib.rs
@@ -3,37 +3,57 @@
 /// Lookup table for day indices.
 const LOOKUP: [usize; 12] = [0, 3, 2, 5, 0, 3, 5, 1, 4, 6, 2, 4];
 
-/// Return the index of the day for which the given date corresponds to.
-///
-/// # Arguments
-/// 
-/// * `y`: Year
-/// * `m`: Month
-/// * `d`: Day
-///
-/// # Example
-///
-/// ```
-/// extern crate dow;
-///
-/// let day_index = dow::dow(2017, 7, 2);
-/// assert_eq!(day_index, 0); // 2017/07/02 is a Sunday
-/// ```
-///
-/// # Note
-///
-///	Sunday is `0`, Monday is `1` etc.
-pub fn dow(y: usize, m: usize, d: usize) -> usize {
+#[derive(Debug, PartialEq)]
+pub enum Day {
+    Sunday,
+    Monday,
+    Tuesday,
+    Wednesday,
+    Thursday,
+    Friday,
+    Saturday
+}
+
+pub fn dow(y: usize, m: usize, d: usize) -> Day {
+    /// Return the index of the day for which the given date corresponds to.
+    ///
+    /// # Arguments
+    ///
+    /// * `y`: Year
+    /// * `m`: Month
+    /// * `d`: Day
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// extern crate dow;
+    /// use dow::Day;
+    ///
+    /// let day_index = dow::dow(2017, 7, 2);
+    /// assert_eq!(day_index, Day::Sunday);
+    /// ```
+
     let mut yy = y;
 
     if m < 3 {
         yy -= 1;
     }
 
-    (yy + yy / 4 - yy / 100 + yy / 400 + LOOKUP[m - 1] + d) % 7
+    let day = (yy + yy / 4 - yy / 100 + yy / 400 + LOOKUP[m - 1] + d) % 7;
+    match day {
+        0 => Day::Sunday,
+        1 => Day::Monday,
+        2 => Day::Tuesday,
+        3 => Day::Wednesday,
+        4 => Day::Thursday,
+        5 => Day::Friday,
+        6 => Day::Saturday,
+        _ => unreachable!()
+    }
 }
+
 
 #[test]
 fn it_works() {
-    assert_eq!(dow(2017, 7, 2), 0);
+    assert_eq!(dow(2017, 7, 2), Day::Sunday);
 }
